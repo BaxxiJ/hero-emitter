0815034ff0b88bfc55cb36d49894e64700374574
Joseph Baxter <jbaxter@redhat.com>
2021-04-01 14:28:44 +0100

    v1.1 -- add multiple apps and endpoints to random timer

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const https = require('https');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; 
 
 var prod_url = "https://hero-superheroes-apicast-production.apps.coffee.demolab.local:443";
-var endpoint = "/v1/api/heroes/random";
-var user_key = "?user_key=d5e22b69792d9c4fe35744df5a7d793c";
+let endpoints = ["/v1/api/heroes/random", "/v1/api/heroes/1", "/v1/api/heroes"];
+let user_keys = ["?user_key=d5e22b69792d9c4fe35744df5a7d793c", "?user_key=2fe5533c885a9a28fd6da5914d47601c"];
 //var test_api = "https://api.kanye.rest/?format=text"
 
 var test_api = {
@@ -17,35 +14,41 @@ var test_api = {
 
 var api = {
     hostname: prod_url,
-    path: endpoint + user_key,
+    path: endpoints[1] + user_keys[1],
 }
 
 // Setting Timers for the function call
 
 // 
+function getRandom(){
+    return (Math.random() * 10) * (Math.random() * 10);
+}
 
-setInterval(timerFunc, 600000);
+setInterval(timerFunc, 1500);
 
-apiRequest(api.hostname, api.path);
+function timerFunc() {
+    user_keys.forEach(apiPathAssembler);
+}
 
-function timerFunc() { 
-    var limit = (Math.random() * 10) * (Math.random() * 10);
-    //var output = "#";
-    for (let i = 0; i < limit; i++) {
-        //console.log(output += "#");
-        apiRequest(api.hostname, api.path);
+function apiPathAssembler(value) {
+    console.log(value);
+    let loops = Math.random() * 3
+    for (let i = 0; i < loops; i++) {
+        let callAmount = getRandom();
+        if (i == 2) {
+            callAmount = callAmount / 4;
+        }
+        for (let j = 0; j < callAmount; j++) {
+            apiRequest(prod_url, endpoints[i] + value);
+            console.log(prod_url, endpoints[i] + value)
+        }
     }
 }
 
-apiRequest(test_api.hostname, test_api.path);
-
 // Sending the API
 
-// apiRequest(api.hostname, api.path);
-// apiRequest(test_api.hostname, test_api.path); 
 
 function apiRequest(hostname, path) {
-    // console.log(hostname + path);
     https.get(hostname + path, (resp) => {
     let data = '';
 
@@ -56,7 +59,6 @@ function apiRequest(hostname, path) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-        //console.log(JSON.parse(data).quote) (.quote removed by path parameter);
         console.log(data); // this basically works as a newline, doesn't output the data object but makes logs nicer
     });
 
